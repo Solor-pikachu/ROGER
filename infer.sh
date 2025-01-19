e6635a2878745c84a1489633dc515e8c3407eeb2
Shoujin Huang <105270555+Solor-pikachu@users.noreply.github.com>
2025-01-19 21:13:31 +0800

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -1,1 +1,1 @@
-python infer.py --input slice_data/meas_MID00274_FID03218_TSE_SMS_243_slice0.npz --output tmp.npz --MB 4 --R 2
+python infer.py --input slice_data/meas_MID00274_FID03218_TSE_SMS_243_slice0.npz --output meas_MID00274_FID03218_TSE_SMS_243_slice0.npz --MB 4 --R 2
